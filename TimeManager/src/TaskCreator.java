795594179971c11b2b03fd239ac7de45ed3beeed
soval_000 <soval_000@Dunkan>
2016-01-02 16:02:44 +0500

    Пересоздал проект, тот что то глючил

---
+++ TimeManager/src/TaskCreator.java
@@ -0,0 +1,19 @@
+
+
+public class TaskCreator {
+
+	/**
+	 * 
+	 */
+	public void addTask() { 
+		// TODO Auto-generated method
+	 }
+
+	/**
+	 * 
+	 */
+	public void deleteTask() { 
+		// TODO Auto-generated method
+	 } 
+
+}
